4cc3c485679870eb6b3b4bb7d9236683d7733a87
Jeff Wong <jeffmjwong@gmail.com>
2019-08-13 20:14:56 +1000

    Server static files in express server using app.use(express.static())

--- server/express.js
+++ server/express.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static('public'));
+
+// app.get('/html', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello world from Express!');
