e8f87d128781f19d3791c4c2a1880837087c901b
Jeff Wong <jeffmjwong@gmail.com>
2019-08-15 20:16:55 +1000

    Serve index.html from public folder from express server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 3001;
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
-  console.log('hello development')
   const config = require('../webpack.config.dev.babel').default;
   const compiler = webpack(config);
 
@@ -16,17 +15,15 @@ if (process.env.NODE_ENV === 'development') {
   }));
 
   app.use(require('webpack-hot-middleware')(compiler));
-} else {
-  console.log('hello production or something else')
 }
 
 // app.get('/html', (req, res) => {
 //   res.sendFile(path.join(__dirname + '/index.html'));
 // });
 
-// app.get('/', (req, res) => {
-//   res.status(200).send('Hello world from Express!');
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}...`);
