4d28bfae0a9854cf16076762d1653c8f3279caf0
Jeff Wong <jeffmjwong@gmail.com>
2019-08-16 20:47:56 +1000

    Use JSON.stringify when setting webpack.DefinePlugin in webpack config

--- webpack.config.dev.babel.js
+++ webpack.config.dev.babel.js
@@ -18,7 +18,7 @@ export default {
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': 'development'
+        'NODE_ENV': JSON.stringify('development')
       }
     })
   ],
