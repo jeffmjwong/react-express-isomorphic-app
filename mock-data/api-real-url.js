718097bd7c2fe78ecd6ea18c2a47a72d19f2383b
Jeff Wong <jeffmjwong@gmail.com>
2019-08-16 20:04:32 +1000

    Rename export constants in api-real-url.js file

--- mock-data/api-real-url.js
+++ mock-data/api-real-url.js
@@ -1,4 +1,4 @@
-export const questions = 'https://api.stackexchange.com/2.0/questions?site=stackoverflow';
-export const question = (id) => (
+export const realQuestionsUrl = 'https://api.stackexchange.com/2.0/questions?site=stackoverflow';
+export const realQuestionUrl = (id) => (
   `https://api.stackexchange.com/2.0/questions/${id}?site=stackoverflow&filter=withbody`
 );
