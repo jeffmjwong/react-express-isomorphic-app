ca396b9758173addddb295fb2682add327e1058e
Jeff Wong <jeffmjwong@gmail.com>
2019-08-17 15:14:33 +1000

    Add mapStateToProps in App.js to map state from redux store

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-export default () => (
+const App = ({ test }) => (
   <div>
     <h2>This is an Isomorphic App with React and Express!</h2>
+    <h3>This is from redux store!! {test}</h3>
   </div>
 );
+
+const mapStateToProps = (state) => ({
+  test: state.test,
+});
+
+export default connect(mapStateToProps)(App);
