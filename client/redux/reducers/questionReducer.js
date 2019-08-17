4ef6b4c9706e51d6e5117aa28eede25237f2c02b
Jeff Wong <jeffmjwong@gmail.com>
2019-08-17 21:24:46 +1000

    Create question reducer function

---
+++ client/redux/reducers/questionReducer.js
@@ -0,0 +1,11 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+export default (state = initialState.questions, action) => {
+  switch (action.type) {
+    case types.FETCH_QUESTIONS_SUCCESS:
+      return action.questions;
+    default:
+      return state;
+  }
+};
