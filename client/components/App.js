c3ece62c2dc574023e0a78587a470b099d512c64
Jeff Wong <jeffmjwong@gmail.com>
2019-08-17 13:46:42 +1000

    Modify client main App.js file

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const App = () => (
+export default () => (
   <div>
     <h2>This is an Isomorphic App with React and Express!</h2>
   </div>
 );
-
-export default App;
