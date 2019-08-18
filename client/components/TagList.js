4d77f98b532668663ec33aa7869411d75ab28d18
Jeff Wong <jeffmjwong@gmail.com>
2019-08-18 14:23:29 +1000

    Create TagList component

---
+++ client/components/TagList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TagList = ({ tags }) => (
+  <div>
+    {
+      tags.map(tag => (
+        <code key={tag}>{tag}</code>
+      ))
+    }
+  </div>
+);
+
+export default TagList;
