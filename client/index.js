83ba5305a9297f1f84a11e2496eddb08a491cb53
Jeff Wong <jeffmjwong@gmail.com>
2019-08-18 14:49:06 +1000

    Implement hot module reload in client/index.js

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import configureStore from './redux/configureStore';
@@ -7,9 +7,20 @@ import App from './components/App';
 
 const store = configureStore();
 
-render(
-  <ReduxProvider store={store}>
-    <App />
-  </ReduxProvider>,
-  document.getElementById('app')
-);
+const render = (Component) => {
+  ReactDOM.render(
+    <ReduxProvider store={store}>
+      <Component />
+    </ReduxProvider>,
+    document.getElementById('app')
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const nextApp = require('./components/App').default;
+    render(nextApp);
+  });
+}
