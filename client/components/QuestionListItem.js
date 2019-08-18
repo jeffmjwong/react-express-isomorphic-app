ecdd4d44d34d37f5faa5233c7e8df771986d374a
Jeff Wong <jeffmjwong@gmail.com>
2019-08-18 14:23:41 +1000

    Render TagList component from QuestionListItem component

--- client/components/QuestionListItem.js
+++ client/components/QuestionListItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import TagList from './TagList';
+
 const QuestionListItem = ({ title, tags }) => (
   <div className='mb-3'>
     <h3>{title}</h3>
 
     <div className='mb-2'>
-      {tags.join(', ')}
+      <TagList tags={tags} />
     </div>
   </div>
 );
