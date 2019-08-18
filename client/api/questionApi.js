faa373f703212e88f982f84d8b6e447eea9e02ce
Jeff Wong <jeffmjwong@gmail.com>
2019-08-18 11:48:37 +1000

    Create getMockQuestions in questionApi.js file

--- client/api/questionApi.js
+++ client/api/questionApi.js
@@ -1,8 +1,20 @@
+const baseUrl = `${process.env.API_URL}api/real-questions`;
+const baseUrlMock = `${process.env.API_URL}api/mock-questions`;
+
 export const getQuestions = async () => {
   try {
-    const response = await fetch('http://localhost:3001/api/real-questions');
+    const response = await fetch(baseUrl);
     return await response.json();
   } catch(err) {
     throw err;
   }
-}
+};
+
+export const getMockQuestions = async () => {
+  try {
+    const response = await fetch(baseUrlMock);
+    return await response.json();
+  } catch(err) {
+    throw err;
+  }
+};
