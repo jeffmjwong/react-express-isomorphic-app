4e991e5fff847d5d49e6742b2885dc32e2981d29
Jeff Wong <jeffmjwong@gmail.com>
2019-08-19 19:08:21 +1000

    Link to individual question in QuestionListItem component

--- client/components/QuestionListItem.js
+++ client/components/QuestionListItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import TagList from './TagList';
 
-const QuestionListItem = ({ title, tags }) => (
+const QuestionListItem = ({ title, tags, question_id }) => (
   <div className='mb-3'>
     <h3>{title}</h3>
 
     <div className='mb-2'>
       <TagList tags={tags} />
+    </div>
+
+    <div>
+      <Link to={`/questions/${question_id}`}>
+        <button>More Info!</button>
+      </Link>
     </div>
   </div>
 );
