f93e93acfe845b8014c7ef7eb346905b17f251f7
Jeff Wong <jeffmjwong@gmail.com>
2019-08-19 19:07:55 +1000

    Link and Route from react-router-dom in App.js

--- client/components/App.js
+++ client/components/App.js
@@ -1,28 +1,36 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Route, Link } from 'react-router-dom';
 
-import * as questionActions from '../redux/actions/questionActions';
+// import * as questionActions from '../redux/actions/questionActions';
 import QuestionList from './QuestionList';
+import QuestionDetail from './QuestionDetail';
 
-const App = ({ questions, fetchQuestions, fetchMockQuestions }) => {
-  useEffect(() => {
-    fetchQuestions();
-  }, [])
+const App = () => {
+  // useEffect(() => {
+  //   fetchQuestions();
+  // }, [])
 
   return (
     <div>
-      <h1 className='mb-5'>This is an Isomorphic App with React and Express!</h1>
+      <h1 className='mb-5'>
+        <Link to='/'>
+          This is an Isomorphic App with React and Express!
+        </Link>
+      </h1>
 
       <div>
-        <QuestionList />
+        <Route exact path='/' component={QuestionList} />
+        <Route exact path='/questions/:id' render={({ match }) => <QuestionDetail question_id={match.params.id} />} />
       </div>
     </div>
   );
 };
 
-const mapDispatchToProps = {
-  fetchQuestions: questionActions.fetchQuestions,
-  fetchMockQuestions: questionActions.fetchMockQuestions,
-};
+// const mapDispatchToProps = {
+//   fetchQuestions: questionActions.fetchQuestions,
+//   fetchMockQuestions: questionActions.fetchMockQuestions,
+// };
 
-export default connect(null, mapDispatchToProps)(App);
+// export default connect(null, mapDispatchToProps)(App);
+export default App;
