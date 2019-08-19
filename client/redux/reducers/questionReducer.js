c2a50766b1d2cacad98a8972fdbcf640bbcbefd6
Jeff Wong <jeffmjwong@gmail.com>
2019-08-19 20:24:14 +1000

    Add reducer to handle FETCH_QUESTION_SUCCESS

--- client/redux/reducers/questionReducer.js
+++ client/redux/reducers/questionReducer.js
@@ -5,6 +5,8 @@ export default (state = initialState.questions, action) => {
   switch (action.type) {
     case types.FETCH_QUESTIONS_SUCCESS:
       return [...action.questions];
+    case types.FETCH_QUESTION_SUCCESS:
+      return [{...action.question}];
     default:
       return state;
   }
