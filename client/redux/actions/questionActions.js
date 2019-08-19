d5d158cf596b8815d1dfa19638e20b5d237b62b6
Jeff Wong <jeffmjwong@gmail.com>
2019-08-19 20:24:31 +1000

    Add new action and new thunk for fetching individual question

--- client/redux/actions/questionActions.js
+++ client/redux/actions/questionActions.js
@@ -1,9 +1,14 @@
 import * as types from './actionTypes';
-import { getQuestions, getMockQuestions } from '../../api/questionApi';
+import { getQuestions, getMockQuestions, getQuestion } from '../../api/questionApi';
 
 export const fetchQuestionsSuccess = (questions) => ({
   type: types.FETCH_QUESTIONS_SUCCESS,
   questions,
+});
+
+export const fetchQuestionSuccess = (question) => ({
+  type: types.FETCH_QUESTION_SUCCESS,
+  question,
 });
 
 export const fetchQuestions = () => (dispatch) => {
@@ -21,6 +26,17 @@ export const fetchMockQuestions = () => (dispatch) => {
   return getMockQuestions()
     .then(data => {
       dispatch(fetchQuestionsSuccess(data.items))
+    })
+    .catch(err => {
+      console.log(err.message);
+      throw err;
+    });
+};
+
+export const fetchQuestion = (id) => (dispatch) => {
+  return getQuestion(id)
+    .then(data => {
+      dispatch(fetchQuestionSuccess(data.items[0]))
     })
     .catch(err => {
       console.log(err.message);
