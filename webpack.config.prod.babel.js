1f1a36e67ba83696977079980d74e46574cbffc0
Jeff Wong <jeffmjwong@gmail.com>
2019-08-20 19:25:34 +1000

    Create webpack.config.prod.babel.js file

---
+++ webpack.config.prod.babel.js
@@ -0,0 +1,34 @@
+import webpack from 'webpack';
+import path from 'path';
+import TerserWebpackPlugin from 'terser-webpack-plugin';
+
+export default {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'client/'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+        'API_URL': JSON.stringify('http://localhost:3001/')
+      }
+    })
+  ],
+  optimization: {
+    minimizer: [new TerserWebpackPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  }
+};
